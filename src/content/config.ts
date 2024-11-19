9e0e80aece6628392d07e61c08f9a05cd91ab27c
Matias Levy <matias.levy.88@gmail.com>
2024-11-19 13:24:09 -0300

    content for our usecase

--- src/content/config.ts
+++ src/content/config.ts
@@ -8,94 +8,38 @@ const productsCollection = defineCollection({
     z.object({
       title: z.string(),
       description: z.string(),
-      main: z.object({
-        id: z.number(),
-        content: z.string(),
-        imgCard: image(),
-        imgMain: image(),
-        imgAlt: z.string(),
-      }),
-      tabs: z.array(
-        z.object({
-          id: z.string(),
-          dataTab: z.string(),
-          title: z.string(),
-        }),
-      ),
-      longDescription: z.object({
-        title: z.string(),
-        subTitle: z.string(),
-        btnTitle: z.string(),
-        btnURL: z.string(),
-      }),
-      descriptionList: z.array(
+      tags: z.array(z.string()),
+      id: z.number(),
+      imgMain: image(),
+      imgMainAlt: z.string(),
+      images: z.array(z.object({ image: image(), imgAlt: z.string() })),
+      youtube: z.string().optional(),
+      manualURL: z.string(),
+      modularGridURL: z.string(),
+
+      highlightsList: z.array(
         z.object({
           title: z.string(),
           subTitle: z.string(),
         }),
       ),
-      specificationsLeft: z.array(
-        z.object({
-          title: z.string(),
-          subTitle: z.string(),
-        }),
-      ),
-      specificationsRight: z
-        .array(
-          z.object({
-            title: z.string(),
-            subTitle: z.string(),
-          }),
-        )
-        .optional(),
-      tableData: z
-        .array(
-          z.object({
-            feature: z.array(z.string()),
-            description: z.array(z.array(z.string())),
-          }),
-        )
-        .optional(),
-      blueprints: z.object({
+      specifications: z.object({
+        "current+12": z.string(),
+        "current-12": z.string(),
+        HP: z.string(),
+        depth: z.string(),
+        inputImpeadence: z.string(),
+        outputImpeadence: z.string(),
+        extra: z.string().optional(),
+      }),
+
+      photosAndVideos: z.object({
         first: image().optional(),
         second: image().optional(),
       }),
     }),
 });
 
-const blogCollection = defineCollection({
-  type: "content",
-  schema: ({ image }) =>
-    z.object({
-      title: z.string(),
-      description: z.string(),
-      contents: z.array(z.string()),
-      author: z.string(),
-      role: z.string().optional(),
-      authorImage: image(),
-      authorImageAlt: z.string(),
-      pubDate: z.date(),
-      cardImage: image(),
-      cardImageAlt: z.string(),
-      readTime: z.number(),
-      tags: z.array(z.string()).optional(),
-    }),
-});
-
-const insightsCollection = defineCollection({
-  type: "content",
-  schema: ({ image }) =>
-    z.object({
-      title: z.string(),
-      description: z.string(),
-      // contents: z.array(z.string()),
-      cardImage: image(),
-      cardImageAlt: z.string(),
-    }),
-});
-
 export const collections = {
   products: productsCollection,
-  blog: blogCollection,
-  insights: insightsCollection,
 };
---
+++ src/content/products/adc.md
@@ -0,0 +1,48 @@
+---
+title: "ADC"
+description: "Introducing the SF-TB T845 – your go-to solution for precision fastening in machinery and equipment. This comprehensive set of machine screws is meticulously crafted to meet the stringent demands of industrial applications, ensuring secure and reliable fastening."
+tags:
+  [
+    "Sample and hold",
+    "Protosequencer",
+    "Gate sequencer",
+    "Noise Source",
+    "Down Sampler",
+  ]
+id: 1
+imgMain: "@/images/blueprint-1.avif"
+imgMainAlt: "asdad"
+images:
+  - image: "@/images/blueprint-1.avif"
+    imgAlt: "asdasd"
+  - image: "@/images/blueprint-1.avif"
+    imgAlt: "asdasd"
+  - image: "@/images/blueprint-1.avif"
+    imgAlt: "asdasd"
+  - image: "@/images/blueprint-1.avif"
+    imgAlt: "asdasd"
+youtube: |
+  <iframe width="560" height="315" src="https://www.youtube.com/embed/pTbrNKWcVrA?si=RG1PbZYkWrXle21L" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+manualURL: "asdf"
+modularGridURL: "asdfad"
+
+highlightsList:
+  - title: "Durability"
+    subTitle: "Crafted from high-quality materials, these machine screws are built to withstand the rigors of industrial environments."
+  - title: "Precision Engineering"
+    subTitle: "Engineered with precision-cut threads and exact specifications, ensuring a tight and secure fit for every application."
+  - title: "Versatility"
+    subTitle: "Suitable for a wide range of machinery and equipment, providing versatile fastening solutions for various industrial needs."
+
+specifications:
+  current+12: "100mA"
+  current-12: "50mA"
+  HP: "6HP"
+  depth: "44mm"
+  inputImpeadence: "Aprox. 100kOhm"
+  outputImpeadence: "1kOhm"
+
+photosAndVideos:
+  first: "@/images/blueprint-1.avif"
+  second: "@/images/blueprint-2.avif"
+---
